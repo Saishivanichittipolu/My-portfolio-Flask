c0003c894565e6441ba455fcf870e94bcbf4b79f
Saishivanichittipolu <149753652+Saishivanichittipolu@users.noreply.github.com>
2024-01-15 20:06:32 +0530

    Update server.py

--- html5up-paradigm-shift/server.py
+++ html5up-paradigm-shift/server.py
@@ -1,11 +1,9 @@
+#import flask 
 from flask import Flask,render_template
 
-
-				
 app=Flask(__name__)
 
-
-
+#path to portfolio
 @app.route('/portfolio')
 def my_portfolio():
     return render_template("index.html")
